1e9c29938991a1197a7fa01956e9beaad8665408
Nathanael <ngreese7@ou.edu>
2021-08-04 18:55:00 -0500

    Added test code for remove user, works correctly

--- Driver.py
+++ Driver.py
@@ -5,10 +5,24 @@ import Grader
 
 def main() :
     grouper = Grouper.Grouper()
-    grouper.create_group(config.GROUP_CATEGORY_ID, "Peussy")
-    sugmas = config.COURSE.get_groups()
-    for sugma in sugmas:
-        print(sugma.name)
+    grouper.create_group(config.GROUP_CATEGORY_ID, "Group 1")
+    violet_user = config.COURSE.get_user(188153)
+    group1 = config.COURSE.get_groups()[0]
+    print(group1.name)
+    grouper.add_members(group1.id, violet_user)
+    users = group1.get_users()
+    for user in users:
+        print(user.name)
+
+    grouper.remove_members(group1.id, violet_user.id)
+
+    print(group1.name)
+
+    users = group1.get_users()
+    for user in users:
+        print(user.name)
+
+    group1.delete()
 
 if __name__ == "__main__":
     main()
