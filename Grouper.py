cecc74e98c6009251a8ba7a67332deeeaf255b22
Nathanael <ngreese7@ou.edu>
2021-08-04 17:38:10 -0500

    Added get students from group id

--- Grouper.py
+++ Grouper.py
@@ -5,6 +5,13 @@ import Grader
 class Grouper:
     def autogroup(self):
     def create_group(self):
-    def get_students_from_group_id(self):
+        
+    def get_students_from_group_id(self, group_id):
+        groups = config.COURSE.get_groups()
+        for group in groups:
+            if (group == group_id):
+                return group.get_users()
+            else:
+                print("Fuck you asshole you gave us the wrong id")
     def add_members(self):
     def remove_members(self):
