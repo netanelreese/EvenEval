ced7c233165b9e6b2788c36bbd03b5f36e34f08c
Nathanael <ngreese7@ou.edu>
2021-08-04 17:54:57 -0500

    Changed formatting

--- Grouper.py
+++ Grouper.py
@@ -12,7 +12,8 @@ class Grouper:
         categories = course.get_group_categories()
         for category in categories:
             if (category == category_id):
-                category.create_group(group={'name': 'Maddie Quiroga'})
+                return category.create_group(group={'name': 'Maddie Quiroga'})
+
     def get_students_from_group_id(self, group_id):
         groups = config.COURSE.get_groups()
         for group in groups:
@@ -24,4 +25,3 @@ class Grouper:
         print()
     def remove_members(self, group_id, user_id):
         print()
-
