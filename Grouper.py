387e570d031626435b50f465d3ef7f5aac763d35
Nathanael <ngreese7@ou.edu>
2021-08-04 18:05:29 -0500

    Adding comments and fixed if statement in get students from group id

--- Grouper.py
+++ Grouper.py
@@ -5,23 +5,23 @@ from canvasapi import *
 import Grader
 
 class Grouper:
-    def autogroup(self):
+    def autogroup(self): #Auto groups each member in a course
         print("sugma balls")
-    def create_group(self, category_id):
-        course = config.COURSE
-        categories = course.get_group_categories()
-        for category in categories:
+    def create_group(self, category_id): #Creates a group within a category id
+        course = config.COURSE #retrieves the course from the config
+        categories = course.get_group_categories() #gets all of the group categories from the course
+        for category in categories: #loops through all of the categories and if one matches the input ID it creates a group inside of it
             id = category.id
-            if (id == category_id):
-                group = category.create_group()
-                group.edit(group={'name': 'Maddie Quiroga'})
+            if (id == category_id): #compares the category id's to the input parameter
+                group = category.create_group() #creates a group
+                group.edit(group={'name': 'Maddie Quiroga'}) #should name the group, isnt
                 print(group)
                 group.delete() #TODO : DELETE THIS
 
     def get_students_from_group_id(self, group_id):
         groups = config.COURSE.get_groups()
         for group in groups:
-            if (group == group_id):
+            if (group.id == group_id):
                 return group.get_users()
             else:
                 print("Fuck you asshole you gave us the wrong id")
