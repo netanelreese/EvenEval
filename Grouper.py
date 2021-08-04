4b1245a8f208c248278050bbab39a1610106d2df
Nathanael <ngreese7@ou.edu>
2021-08-04 17:44:49 -0500

    added create_group

--- Grouper.py
+++ Grouper.py
@@ -1,11 +1,17 @@
+from canvasapi.group import GroupCategory
+
 import config
 from canvasapi import *
 import Grader
 
 class Grouper:
     def autogroup(self):
-    def create_group(self):
-        
+    def create_group(self, category_id):
+        course = config.COURSE
+        categories = course.get_group_categories()
+        for category in categories:
+            if (category == category_id):
+                category.create_group(group={'name': 'Maddie Quiroga'})
     def get_students_from_group_id(self, group_id):
         groups = config.COURSE.get_groups()
         for group in groups:
@@ -13,5 +19,5 @@ class Grouper:
                 return group.get_users()
             else:
                 print("Fuck you asshole you gave us the wrong id")
-    def add_members(self):
-    def remove_members(self):
+    def add_members(self, group_id, user_id):
+    def remove_members(self, group_id, user_id):
