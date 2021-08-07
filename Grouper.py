82733ec41e35a129eb957350df46a4fe4992dea2
Nathanael <ngreese7@ou.edu>
2021-08-07 17:04:11 -0500

    Added comments

--- Grouper.py
+++ Grouper.py
@@ -10,9 +10,9 @@ class Grouper:
         i = 0
         added = 0
 
-        users = config.COURSE.get_users(enrollment_type=['student'])
+        users = config.COURSE.get_users(enrollment_type=['student']) #gets all users of type student
         class_floor = self.cal_user_num(class_num, group_num)
-        while (i < group_num):
+        while (i < group_num): #this while loop groups each student in even number groups
             group_name = "Group " + str(i)
             group = self.create_group(category_id, group_name)
             j = 0
@@ -38,19 +38,19 @@ class Grouper:
                 group = category.create_group() #creates a group
                 group.edit(name=group_name) #should name the group, isnt
         return group
-    def get_students_from_group_id(self, group_id):
+    def get_students_from_group_id(self, group_id): #Gets a student from a certain input id
         groups = config.COURSE.get_groups()
         for group in groups:
             if (group.id == group_id):
                 return group.get_users()
             else:
                 print("Fuck you asshole you gave us the wrong id")
-    def add_members(self, group_id, user_id):
+    def add_members(self, group_id, user_id): #Adds a member to a specified group, using the group id and user id
         groups = config.COURSE.get_groups()
         for group in groups:
             if (group.id == group_id):
                 group.create_membership(user_id)
-    def remove_members(self, group_id, user_id):
+    def remove_members(self, group_id, user_id): #removes a member from a specified group using the group and user id
         groups = config.COURSE.get_groups()
         for group in groups:
             if (group.id == group_id):
@@ -58,13 +58,13 @@ class Grouper:
                 for user in users:
                     if (user.id == user_id):
                         group.remove_user(user)
-    def find_user(self, user_id):
+    def find_user(self, user_id): #find's a user given a specific id
         users = config.COURSE.get_users()
         for user in users:
             if (user.id == user_id):
                 return True
         return False
-    def cal_user_num(self, class_num, group_num):
+    def cal_user_num(self, class_num, group_num): #calculates the number of users per each group in a course
         flor = floor(class_num/group_num)
         return flor
     def get_user_ids(self):
