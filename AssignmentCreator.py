7bb8ac78c65903fc27ffa9818472dfb1388417ff
{name} <{nickstoia1@ou.edu}>
2021-09-02 08:08:11 -0500

    Got quiz to work for only visible to overrides. Now to figure out how to do overrides

--- AssignmentCreator.py
+++ AssignmentCreator.py
@@ -4,40 +4,46 @@ import config
 from canvasapi import *
 
 class AssignmentCreator:
+    y = 7
 
-    def createQuiz (self, studentName, groupid):
+    def createQuiz (self, studentName, id):
 
-        quiz = config.COURSE.create_quiz({
-            'title' : studentName,
-            'assignment_group_id' : 1,
-            'for': [188153]
+        assignment = config.COURSE.create_quiz(quiz={
+            'title': studentName,
+            'quiz_type': 'assignment',
+            'only_visible_to_overrides': True,
+            'quiz_assignment_overrides': [{
+                'student_ids' : [id]
+            }]
         })
-        quiz.create_question(question={
-            'question_name': 'Gender',
-            'question_type': 'multiple_choice_question',
-            'question_text': 'Which gender do you identify as?',
-            'points_possible': '100.0',
-            'answers': [{'answer_text':'Male', 'weight':100.0}
-                ,{'answer_text':'Female', 'weight':0.0}
-                ,{'answer_text':'Other', 'weight':0.0}
-                ,{'answer_text':'Prefer not to say', 'weight':0.0}
-                ]
-        })
-        return quiz
+        
+        assignment.create_question(question = {
+                'question_name': 'Gender',
+                'question_type': 'multiple_choice_question',
+                'question_text': 'Which gender do you identify as?',
+                'points_possible': '100.0',
+                'answers': [{'answer_text': 'Male', 'weight': 100.0}
+                        , {'answer_text': 'Female', 'weight': 0.0}
+                        , {'answer_text': 'Other', 'weight': 0.0}
+                        , {'answer_text': 'Prefer not to say', 'weight': 0.0}
+                ]})
+        return assignment
     def generateIndQuizzes(self):
 
         groups = config.COURSE.get_groups()
         for group in groups:
-            module = config.COURSE.create_module({'name':group.name})
+            module = config.COURSE.create_module({'name':group.name, 'published':True})
             users = group.get_users(enrollment_type=['student'])
             for user in users:
-                currName = user.name + ' Evaluation'
-                quiz = self.createQuiz(currName, group.id)
-                module.create_module_item({'title':quiz.title,'type':'Quiz', 'content_id':quiz.id})
+                currName = user.name + ' Evaluation ' + str(self.y)
+                self.y = self.y +1
+                assignment = self.createQuiz(currName, user.id)
+                #quiz = self.createQuiz(currName, group.id)
+                module.create_module_item({'title':assignment.title,'type':'Quiz', 'content_id':assignment.id})
                 for other in users:
-                    if (user.id == other.id):
-                        x = user.id
-                        quiz.create_submission(submission={'user_id':x, 'excused':True})
+                    if (user.id != other.id):
+                        x=1
+
 
 
                 #quiz.edit({'assignment_visibility':self.assignQuiz(quiz, user, group)})
