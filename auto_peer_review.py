5dfe5e6274e5d987a1c17b09abee8475af7bb142
{name} <{nickstoia1@ou.edu}>
2021-09-02 16:13:46 -0500

    create method to create assignment and assign peer reviews

--- auto_peer_review.py
+++ auto_peer_review.py
@@ -1,12 +1,16 @@
 import config
 import canvasapi
 import Grouper
-import nltk
 
-nltk.download()
 
 class APR :
     def create_assn(self):
+        assignment = config.COURSE.create_assignment({
+            'name' : 'Peer Review 1',
+            'peer_reviews': True,
+        })
+        self.generate_peer_reviews(assignment)
+
         print()
 
     def edit_reviewer_count(self):
@@ -18,8 +22,14 @@ class APR :
     def assign_APR_to_group(self):
         print()
 
-    def generate_peer_reviews(self):
-        print()
+    def generate_peer_reviews(self, assignment):
+        groups = config.COURSE.get_groups()
+        for group in groups:
+            users = group.get_users()
+            for student1 in users:
+                for student2 in users:
+                    if (student1.id != student2.id):
+                        assignment.get_submission(student1.id).create_submission_peer_review(student2.id)
 
     def sentiment_analysis(self):
         print()
