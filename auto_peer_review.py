975648ec2fc99bfab97b53e8f4f4b5355de424c0
nickstoia1 <nickstoia1@ou.edu>
2021-09-02 20:11:02 -0500

    make peer reviews anonymous

--- auto_peer_review.py
+++ auto_peer_review.py
@@ -8,6 +8,7 @@ class APR :
         assignment = config.COURSE.create_assignment({
             'name' : 'Peer Review 1',
             'peer_reviews': True,
+            'anonymous_peer_reviews':True
         })
         self.generate_peer_reviews(assignment)
         x=1
@@ -29,7 +30,7 @@ class APR :
             for student1 in users:
                 for student2 in users:
                     if (student1.id != student2.id):
-                        assignment.get_submission(student1.id).create_submission_peer_review(student2.id)
+                        peer_review = assignment.get_submission(student1.id).create_submission_peer_review(student2.id)
 
     def sentiment_analysis(self):
         print()
