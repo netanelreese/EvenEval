69efbc5b2ebda672ed8cf5f7f6634e6c96bc60f5
nickstoia1 <nickstoia1@ou.edu>
2021-09-10 13:38:50 -0500

    Updated for getting the peer reviews and added Sentiment Intensity Analyzer

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,14 +2,25 @@ import nltk
 from nltk import *
 from nltk.corpus import wordnet
 from nltk import PorterStemmer
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+import config
+
 
 class sentiment_analysis():
+
+    def getSubmissions(self, assignmentID):
+        assignment = config.COURSE.get_assignment(assignmentID)
+        users = config.COURSE.get_users()
+        for user in users:
+            peer_reviews = assignment.get_submission(user.id).get_submission_peer_reviews()
 
     def train(self):
         print()
 
-    def analyze(self):
-        print()
+    def analyze(self, text):
+        sia = SentimentIntensityAnalyzer()
+        sia.polarity_scores(text)
 
     def read_text(self):
         print()
