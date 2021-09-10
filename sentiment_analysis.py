301d07a9d42137382a623581bb24f053a44f5975
nickstoia1 <nickstoia1@ou.edu>
2021-09-10 13:59:15 -0500

    Got peer reviews. Looking for how to get the text of the peer review

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -11,9 +11,15 @@ class sentiment_analysis():
 
     def getSubmissions(self, assignmentID):
         assignment = config.COURSE.get_assignment(assignmentID)
-        users = config.COURSE.get_users()
+        users = config.COURSE.get_users(enrollment_type=['student'])
         for user in users:
-            peer_reviews = assignment.get_submission(user.id).get_submission_peer_reviews()
+            peer_reviews = assignment.get_submission(user).get_submission_peer_reviews()
+            num_reviews = 0
+            for peer_review in peer_reviews:
+                num_reviews = num_reviews+1
+                
+
+
 
     def train(self):
         print()
