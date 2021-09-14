6ef9a7a344425d406c9a147a607b0f523de86891
nickstoia1 <nickstoia1@ou.edu>
2021-09-14 14:58:15 -0500

    Updated with positive or negative assignment description

--- auto_peer_review.py
+++ auto_peer_review.py
@@ -4,18 +4,28 @@ import Grouper
 
 
 class APR :
-    def create_assn(self, assignment_name):
+    def create_assn(self, assignment_name, positive_negative):
+        positive = '<p>For this assignment test, I want you to write something really positive in the peer review. ' \
+                   '<br> This can include: <ul> <li>"Great work."</li> <li> "He is the BEST."</li> </ul> <br> The ' \
+                   'analysis takes all caps into consideration so use them for emphasis. Please write at least ' \
+                   'a couple of sentences about how great something is. We are trying to see if the code can '\
+                   'make a good positive analysis and assign the grade accordingly.'
+        negative = '<p>For this assignment test, I want you to write something really negative in the peer review. ' \
+                   '<br> This can include: <ul> <li>"Terrible work."</li> <li> "He is the WORST."</li> </ul> <br> The ' \
+                   'analysis takes all caps into consideration so use them for emphasis. Please write at least ' \
+                   'a couple of sentences about how bad something is. We are trying to see if the code can '\
+                   'make a good negative analysis and assign the grade accordingly.'
+        if (positive_negative == 'P'):
+            description = positive
+        else:
+            description = negative
         assignment = config.COURSE.create_assignment({
             'name': assignment_name,
             'peer_reviews': True,
             'anonymous_peer_reviews':True,
             'submission_types':'none',
             'points_possible':10,
-            'description':'<p>For this assignment test, I want you to write something really positive in the peer '
-                          'review. <br> This can include: <ul> <li>"Great work."</li> <li> "He is the BEST."</li> </ul> <br> The '
-                          'analysis takes all caps into consideration so use them for emphasis. Please write at least '
-                          'a couple of sentences about how great something is. We are trying to see if the code can '
-                          'make a good positive analysis and assign the grade accordingly.'
+            'description': description
         })
         self.generate_peer_reviews(assignment)
 
