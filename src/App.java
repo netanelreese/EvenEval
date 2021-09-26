76efd7340312a8f5ccd3352bf8e40b4e7aba3991
Nathanael <ngreese7@ou.edu>
2021-09-26 17:57:08 -0500

    Making applet look pretty

--- src/App.java
+++ src/App.java
@@ -19,16 +19,66 @@ import javax.swing.*;
  */
 public class App extends JFrame implements ActionListener {
 
-	private Color crimson = new Color(132, 22, 23, 1);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	JButton createAssn;
+	JLabel selectOption = new JLabel("Please select an option below:");
 	JButton gradeAssn;
 	JTextField assnID;
+	JLabel title = new JLabel("EvenEval");
+	JLabel company = new JLabel("/src/");
+	
 	
 	public void launch() {
 		setTitle("EvenEval");
+		
+
+		
 		createAssn = new JButton("Create an Assignment");
 		gradeAssn = new JButton("Grade an Assignment");
 		setPreferredSize(new Dimension(400, 400));
+	    setLayout(new GridBagLayout());
+	    GridBagConstraints positionConst = new GridBagConstraints();
+		getContentPane().setBackground( new Color(132, 22, 23));
+		
+		Font labelFont = new Font("Comic Sans", Font.BOLD, 14);
+		
+		title.setFont(labelFont);
+		title.setForeground(Color.WHITE);
+		
+		company.setFont(labelFont);
+		company.setForeground(Color.WHITE);
+		
+		selectOption.setForeground(Color.WHITE);
+		
+		positionConst.insets = new Insets(5, 5, 5, 5);
+	    positionConst.gridx = 0;
+	    positionConst.gridy = 0;
+	    add(title, positionConst);
+	    
+		positionConst.insets = new Insets(5, 5, 5, 5);
+	    positionConst.gridx = 1;
+	    positionConst.gridy = 0;
+	    add(company, positionConst);
+	    
+		positionConst.insets = new Insets(5, 5, 5, 5);
+	    positionConst.gridx = 0;
+	    positionConst.gridy = 1;
+	    add(selectOption, positionConst);
+	    
+		positionConst.insets = new Insets(5, 5, 5, 5);
+	    positionConst.gridx = 0;
+	    positionConst.gridy = 2;
+	    add(createAssn, positionConst);
+	    
+		positionConst.insets = new Insets(5, 5, 5, 5);
+	    positionConst.gridx = 1;
+	    positionConst.gridy = 2;
+	    add(gradeAssn, positionConst);
+	    
+		pack();
 	}
 	
 	@Override
--- src/Driver.java
+++ src/Driver.java
@@ -3,12 +3,12 @@ import javax.swing.JFrame;
 public class Driver {
 
 	public static void main(String[] args) {
-		App app = new App(); //creating app instance
-		app.launch(); //launching the splash applicaton
+		App EvenEval = new App(); //creating app instance
+		EvenEval.launch(); //launching the splash applicaton
 
-	    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //terminates program on close
-	    app.pack(); //making program look good
-	    app.setVisible(true); //setting application visible
+	    EvenEval.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //terminates program on close
+	    EvenEval.pack(); //making program look good
+	    EvenEval.setVisible(true); //setting application visible
 	}
 
 }
