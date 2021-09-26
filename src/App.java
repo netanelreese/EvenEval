a41c2436d828988b0c4a8c9841974a2c31dccfa0
Nathanael <ngreese7@ou.edu>
2021-09-26 18:30:32 -0500

    Created popup for grading an assignment

--- src/App.java
+++ src/App.java
@@ -29,6 +29,8 @@ public class App extends JFrame implements ActionListener {
 	JTextField assnID;
 	JLabel title = new JLabel("EvenEval");
 	JLabel company = new JLabel("/src/");
+	Font labelFont = new Font("Comic Sans", Font.BOLD, 25);
+	Color cream = new Color(255, 241, 208);
 	
 	
 	public void launch() {
@@ -43,15 +45,15 @@ public class App extends JFrame implements ActionListener {
 	    GridBagConstraints positionConst = new GridBagConstraints();
 		getContentPane().setBackground( new Color(132, 22, 23));
 		
-		Font labelFont = new Font("Comic Sans", Font.BOLD, 25);
+
 		
 		title.setFont(labelFont);
-		title.setForeground(Color.WHITE);
+		title.setForeground(cream);
 		
 		company.setFont(labelFont);
-		company.setForeground(Color.WHITE);
+		company.setForeground(cream);
 		
-		selectOption.setForeground(Color.WHITE);
+		selectOption.setForeground(cream);
 		
 		positionConst.insets = new Insets(5, 5, 5, 5);
 	    positionConst.gridx = 0;
@@ -99,12 +101,50 @@ public class App extends JFrame implements ActionListener {
 	
 	private class ButtonClickListener implements ActionListener{
 	      public void actionPerformed(ActionEvent e) {
-	         String command = e.getActionCommand();  
+	         String command = e.getActionCommand();
+	         String enterAssnID = "Enter Assignment ID";
 	         
 	         if( command.equals( "Grade an Assignment" ))  {
-	        	 System.out.println("Grade");
+		        	
+		            JFrame popup = new JFrame("Enter Assignment ID");
+		            
+		            JLabel enterID = new JLabel(enterAssnID);
+		            JButton ok = new JButton("OK");
+		            JTextField assnID = new JTextField();
+		            assnID.setEditable(true);
+		            assnID.setPreferredSize(new Dimension(200, 35));
+		            
+		            
+		            popup.setVisible(true);
+		            popup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		            
+		    		popup.setPreferredSize(new Dimension(400, 400));
+		    	    popup.setLayout(new GridBagLayout());
+		    	    GridBagConstraints popupPositionConst = new GridBagConstraints();
+		    		popup.getContentPane().setBackground( new Color(132, 22, 23));
+		    		
+		    		
+		    		enterID.setForeground(cream);
+		    		Font labelFont = new Font("Comic Sans", Font.BOLD, 14);
+		    		
+		    		popupPositionConst.insets = new Insets(5, 5, 5, 5);
+		    		popupPositionConst.gridx = 0;
+		    	    popupPositionConst.gridy = 0;
+		    	    popup.add(enterID, popupPositionConst);
+		    	    
+		    		popupPositionConst.insets = new Insets(5, 5, 5, 5);
+		    		popupPositionConst.gridx = 0;
+		    	    popupPositionConst.gridy = 1;
+		    	    popup.add(assnID, popupPositionConst);
+		    	    
+		    	    popupPositionConst.insets = new Insets(5, 5, 5, 5);
+		    		popupPositionConst.gridx = 0;
+		    		popupPositionConst.gridy = 2;
+		    	    popup.add(ok, popupPositionConst);
+		    	    
+		    	    popup.pack();
 	         } 
-	         else if( command.contains( "Create" ) )  {
+	         else if(command.contains( "Create" ) )  {
 	            System.out.println("Create");
 	         }
 	      }		
