b232fac0922e6f518651a5467c2d63a2a8c28430
Nathanael <ngreese7@ou.edu>
2021-09-30 15:14:24 -0500

    how the fuck do i need to import this shit????

--- src/org/python/core/Visitproc.java
+++
@@ -1,9 +0,0 @@
-package org.python.core;
-
-public interface Visitproc {
-
-    /**
-     * Must not be called with {@code object == null}.
-     */
-    public int visit(PyObject object, Object arg);
-}
