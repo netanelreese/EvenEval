2a62e0181746fbcdb568fad29e0226f11795d4c1
nickstoia1 <nickstoia1@ou.edu>
2022-01-20 14:49:22 -0600

    Updated based off running the code

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ with open('config.toml', 'r') as file:
 
 
 API_KEY = lines[0]
-GROUP_CATEGORY_ID = lines[3]
+GROUP_CATEGORY_ID = 1111
 COURSE_ID = lines[2]
 API_URL = lines[1]
 CANVAS = Canvas(API_URL, API_KEY)
