de704e867c3110ac058627cf7320f633c815e715
nickstoia1 <nickstoia1@ou.edu>
2022-01-20 14:46:16 -0600

    Gets the groups and writes to the config.toml

--- GroupGetter.py
+++ GroupGetter.py
@@ -2,8 +2,22 @@ import config
 from canvasapi import *
 
 def main():
-     groups = config.COURSE.get_group_categories()
-     for group in groups:
+     with open('config.toml', 'r') as file:
+          curr = file.readlines()
+          while curr.__len__() > 3:
+               curr.pop()
+          file.close()
+     with open('config.toml', 'w') as file:
+          file.writelines(curr)
+          file.close()
+     with open('config.toml', 'a') as file:
+          lines = []
+          group_cats = config.COURSE.get_group_categories()
+          for group_cat in group_cats:
+               cat = group_cat.name + " " + str(group_cat.id) + '\n'
+               lines.append(cat)
+          file.writelines(lines)
+          file.close()
 
 
 if __name__ == "__main__":
